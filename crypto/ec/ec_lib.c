61415596a85ba05f75af4384a79db33b4b814bf9
rpm-build <rpm-build>
2024-03-06 19:17:15 +0100

    RH: Disable explicit ec curves
    Patch-name: 0012-Disable-explicit-ec.patch
    Patch-id: 12
    Patch-status: |
        # # Disable explicit EC curves
        # # https://bugzilla.redhat.com/show_bug.cgi?id=2066412
    From-dist-git-commit: 4334bc837fbc64d14890fdc51679a80770d498ce

--- crypto/ec/ec_lib.c
+++ crypto/ec/ec_lib.c
@@ -1728,6 +1728,11 @@ EC_GROUP *EC_GROUP_new_from_params(const OSSL_PARAM params[],
         goto err;
     }
     if (named_group == group) {
+        if (EC_GROUP_check_named_curve(group, 0, NULL) == NID_undef) {
+            ERR_raise(ERR_LIB_EC, EC_R_UNKNOWN_GROUP);
+            goto err;
+        }
+#if 0
         /*
          * If we did not find a named group then the encoding should be explicit
          * if it was specified
@@ -1743,6 +1748,7 @@ EC_GROUP *EC_GROUP_new_from_params(const OSSL_PARAM params[],
             goto err;
         }
         EC_GROUP_set_asn1_flag(group, OPENSSL_EC_EXPLICIT_CURVE);
+#endif
     } else {
         EC_GROUP_free(group);
         group = named_group;
