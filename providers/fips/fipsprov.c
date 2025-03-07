9e4d1045adcc46534a4131c01225147a0bf54a01
Simo Sorce <simo@redhat.com>
2025-03-07 18:15:13 -0500

    FIPS: NO DES support
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- providers/fips/fipsprov.c
+++ providers/fips/fipsprov.c
@@ -380,7 +380,8 @@ static const OSSL_ALGORITHM_CAPABLE fips_ciphers[] = {
          ossl_cipher_capable_aes_cbc_hmac_sha512_etm),
     ALGC(PROV_NAMES_AES_256_CBC_HMAC_SHA512_ETM, ossl_aes256cbc_hmac_sha512_etm_functions,
          ossl_cipher_capable_aes_cbc_hmac_sha512_etm),
-#ifndef OPENSSL_NO_DES
+/* We don't certify 3DES in our FIPS provider */
+#if 0 /* ifndef OPENSSL_NO_DES */
     ALG(PROV_NAMES_DES_EDE3_ECB, ossl_tdes_ede3_ecb_functions),
     ALG(PROV_NAMES_DES_EDE3_CBC, ossl_tdes_ede3_cbc_functions),
 #endif  /* OPENSSL_NO_DES */
