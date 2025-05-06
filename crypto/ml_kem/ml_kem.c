96a686601ac834dd57bc684d0118f2cc98d52f6f
Simo Sorce <simo@redhat.com>
2025-05-06 13:18:14 -0400

    RH: Zeroize PSPs for ML-DSA and ML-KEM
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- crypto/ml_kem/ml_kem.c
+++ crypto/ml_kem/ml_kem.c
@@ -1601,6 +1601,10 @@ void ossl_ml_kem_key_reset(ML_KEM_KEY *key)
     if (key->t != NULL) {
         if (ossl_ml_kem_have_prvkey(key))
             OPENSSL_secure_clear_free(key->s, key->vinfo->prvalloc);
+
+        /* zeroize PSPs */
+        OPENSSL_cleanse(key->t, key->vinfo->puballoc);
+
         OPENSSL_free(key->t);
     }
     key->d = key->z = key->seedbuf = key->encoded_dk = (uint8_t *)(key->s = key->m = key->t = NULL);
