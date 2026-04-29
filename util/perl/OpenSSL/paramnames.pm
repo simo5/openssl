c6c94fe757eebef4ceb820b940e091c59111f39e
Simo Sorce <simo@redhat.com>
2026-04-29 17:44:44 -0400

    Add FIPS indicator for RSA raw signatures
    Add the `rh-raw-signature-check` FIPS indicator to restrict unapproved raw RSA
    and ECDSA signature operations. Under FIPS 140-3, RSA and ECDSA signature
    verification requires the digest to be verified by the primitive, and for RSA
    signature generation with padding requires a digest. Operations lacking a
    digest are unapproved and will now correctly trigger this indicator.
    
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- util/perl/OpenSSL/paramnames.pm
+++ util/perl/OpenSSL/paramnames.pm
@@ -61,6 +61,7 @@ my %params = (
     'OSSL_PROV_PARAM_ECDH_COFACTOR_CHECK' =>    "ecdh-cofactor-check",    # uint
     'OSSL_PROV_PARAM_SIGNATURE_DIGEST_CHECK' => "signature-digest-check", # uint
     'OSSL_PROV_PARAM_RH_TEST_FACILITIES_CHECK' => "rh-test-facitlities-check", # uint
+    'OSSL_PROV_PARAM_RH_RAW_SIGNATURE_CHECK' => "rh-raw-signature-check", # uint
 
 # Self test callback parameters
     'OSSL_PROV_PARAM_SELF_TEST_PHASE' =>  "st-phase",# utf8_string
